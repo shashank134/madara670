5bf3b939dcfb464f83ea4b5f981aede17761c99b
shashank134 <75831097+shashank134@users.noreply.github.com>
2024-01-20 13:24:43 +0530

    Update boom.js

--- boom.js
+++ boom.js
@@ -1,3 +1,16 @@
 // File: alertScript.js
 
-alert("Hello, this is an alert!");
+alert(document.domain);
+// File: script.js
+
+// Example AJAX request to a PHP endpoint
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://shashank134.github.io/madara670/boom.html', true);
+
+xhr.onload = function () {
+    if (xhr.status == 200) {
+        alert(xhr.responseText);
+    }
+};
+
+xhr.send();
