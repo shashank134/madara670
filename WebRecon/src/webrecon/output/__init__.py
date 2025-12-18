5611cd1691e09ab516930bed95e10b19a19df28e
shashank134 <75831097+shashank134@users.noreply.github.com>
2025-12-18 09:21:49 +0530

    Delete WebRecon directory

--- WebRecon/src/webrecon/output/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Output formatters for WebRecon."""
-
-from .json_output import JSONOutputHandler
-from .html_output import HTMLOutputHandler
-
-__all__ = ["JSONOutputHandler", "HTMLOutputHandler"]
