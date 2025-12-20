fbe20171105a327f9c47333c8dd346d2a243cb5e
shashank134 <75831097+shashank134@users.noreply.github.com>
2025-12-20 09:07:15 +0530

    Delete ReconJsHunter directory

--- ReconJsHunter/src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-ReconHunter - Professional Bug Bounty Reconnaissance Tool
-A modular OSINT and JavaScript intelligence framework for authorized security testing.
-"""
-
-__version__ = "1.0.0"
-__author__ = "Security Research Team"
